4ef8d989f5d86146ca4d2d678d27b002fa436d2f
mischadab <mischadabrowski@gmail.com>
2021-03-18 18:31:45 -0400

    main for dataarea

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DataArea from '../DataArea/DataArea'
+import './Main.css'
+
+function Main () {
+    return (
+        <div>
+            <DataArea/>
+        </div>
+    )
+}
+
+export default Main
