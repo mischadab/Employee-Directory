a58438c5546cb6d37833efb55b32c5b98df675a5
mischadab <mischadabrowski@gmail.com>
2021-03-19 15:19:15 -0400

    added columns for all info

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import './Body.css'
 
-function Body({users}) {
+function Body(props) {
     
     return (
     <tbody>
-        { users[0] !==undefined && users[0].name ? (
-            users.map(({ login, name, picture, phone, email, bd }) => {
+        { props.users[0] !==undefined && props.users[0].name ? (
+            props.users.map(user => {
                 return (
-                    <tr key={login.uuid}>
+                    <tr key={user.login.uuid}>
                         <td className="align-middle">
-                            
+                        <img
+                        src={user.picture.medium}
+                        className="img-responsive"/>
                         </td>
+                        <td>
+                            {user.name.first} {user.name.last}
+                        </td>
+                        <td>
+                            {user.phone}
+                        </td>
+                        <td>
+                            {user.email}
+                        </td>
+
                     </tr>
                 )
             })
