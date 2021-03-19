1e09eeae95479e432a3de1eba147982ffa527bca
mischadab <mischadabrowski@gmail.com>
2021-03-19 15:19:47 -0400

    component did mount and fixed handle search change

--- src/components/DataArea/DataArea.jsx
+++ src/components/DataArea/DataArea.jsx
@@ -1,23 +1,55 @@
 import React, { Component } from 'react'
 import Navbar from '../Navbar/Navbar'
 import API from '../../utils/API'
-import './DataArea.css'
-import DataAreaContext from '../../utils/DataAreaContext'
+import DataTable from '../DataTable/DataTable'
+
 
 class DataArea extends Component {
+    state={
+        users:[{}],
+        filteredUsers:[{}]
+    }
+    headings=[
+        {name:"Image",width:"10%"},
+        {name:"Name",width:"10%"},
+        {name:"Phone",width:"10%"},
+        {name:"Email",width:"10%"},
+        {name:"DOB",width:"10%"},
+        {}
 
+    ]
+    componentDidMount(){
+API.getUsers().then(results=>{
+    console.log(results.data.results)
+    this.setState({
+        users:results.data.results,
+        filteredUsers:results.data.results
+    })
+})
+
+}
+
+handleSort=heading=>{
+    
+}
     handleSearchChange = event => {
         const filter = event.target.value;
-        const filteredList = developerState.users.filter( item => { 
+        const filteredList = this.state.users.filter( item => { 
             let values = item.name.first.toLowerCase();
             return values.indexOf(filter.toLowerCase()) !== -1;
         })
+        this.setState({filteredUsers:filteredList})
     }
 
     render() {
         return (
             <div>
-                <Navbar/>
+                <Navbar handleSearchChange={this.handleSearchChange}/>
+                <DataTable
+                headings={this.headings}
+                users={this.state.filteredUsers}
+                handleSort={this.handleSort}
+                />
             </div>
         )
     }
