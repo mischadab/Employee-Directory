39e38d782fd43f6eb3ed6f3a58f3ded73267e2ac
mischadab <mischadabrowski@gmail.com>
2021-03-19 15:20:29 -0400

    props for search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Search.css'
+import DataAreaContext from '../../utils/DataAreaContext'
 
-function Search( { handleSearchChange } ) {
+function Search( props ) {
+  
     return (
         <div className='searchbox'>
             <form className="form-inline">
-                <input type="search" className="form-control" placeholder="Type here" onChange={ e => handleSearchChange(e) } />
+                <input type="search" className="form-control" placeholder="Type here" onChange={ e => props.handleSearchChange(e) } />
+                <button className='btn' type='submit'>Search</button>
             </form>
         </div>
     )
