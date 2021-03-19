f617299dd470c1a4599c8e0a3d1cbc8a8f30c507
mischadab <mischadabrowski@gmail.com>
2021-03-18 20:09:53 -0400

    handlesearchchange function added

--- src/components/DataArea/DataArea.jsx
+++ src/components/DataArea/DataArea.jsx
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
 import Navbar from '../Navbar/Navbar'
+import API from '../../utils/API'
 import './DataArea.css'
+import DataAreaContext from '../../utils/DataAreaContext'
 
 class DataArea extends Component {
+
+    handleSearchChange = event => {
+        const filter = event.target.value;
+        const filteredList = developerState.users.filter( item => { 
+            let values = item.name.first.toLowerCase();
+            return values.indexOf(filter.toLowerCase()) !== -1;
+        })
+    }
+
     render() {
         return (
             <div>
