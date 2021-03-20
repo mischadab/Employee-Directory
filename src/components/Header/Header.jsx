ad518f0bc1afdd1f6fede60a7946eca67318f279
mischadab <mischadabrowski@gmail.com>
2021-03-20 15:52:13 -0400

    new header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './Header.css'
 
-class Header extends Component {
-    render() {
-        return (
-            <div className='header'>
-                <h1>Employee Directory</h1>
-                <p>Use the arrows or the search box for results</p>
-            </div>
-        )
-    }
+function Header() {
+    return (
+        <div>
+        <div className="jumbotron jumbotron-fluid" id="jumboTron">
+        <div className="container">
+            <h1>Employee Directory</h1>
+            <p>Search for an Employee by first or last name, or select a column heading to sort</p>
+        </div>
+        </div>
+        </div>
+    )
 }
 
-export default Header
+export default Header;
