08a1ef46d166d047c888aec622c0f245a6ade20b
mischadab <mischadabrowski@gmail.com>
2021-03-20 18:19:21 -0400

    searchbar

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Search.css';
 
-const Search( props ) {
+const Search = ( props ) => {
     return (
         <form>
             <div className="form-group search-widget">
@@ -20,3 +20,5 @@ const Search( props ) {
         </form>
     )
 }
+
+export default Search;
