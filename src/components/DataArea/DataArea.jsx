45946237af2dbb603c0f6ea366958b2b59b60c4f
mischadab <mischadabrowski@gmail.com>
2021-03-20 15:12:42 -0400

    handle sorting

--- src/components/DataArea/DataArea.jsx
+++ src/components/DataArea/DataArea.jsx
@@ -5,50 +5,60 @@ import DataTable from '../DataTable/DataTable'
 
 
 class DataArea extends Component {
-    state={
-        users:[{}],
-        filteredUsers:[{}]
+    state = {
+        users: [{}],
+        filteredUsers: [{}]
     }
-    headings=[
-        {name:"Image",width:"10%"},
-        {name:"Name",width:"10%"},
-        {name:"Phone",width:"10%"},
-        {name:"Email",width:"10%"},
-        {name:"DOB",width:"10%"},
+    headings = [
+        { name: "Image", width: "10%" },
+        { name: "Name", width: "10%" },
+        { name: "Phone", width: "10%" },
+        { name: "Email", width: "10%" },
+        { name: "DOB", width: "10%" },
         {}
 
     ]
-    componentDidMount(){
-API.getUsers().then(results=>{
-    console.log(results.data.results)
-    this.setState({
-        users:results.data.results,
-        filteredUsers:results.data.results
-    })
-})
 
-}
+    componentDidMount() {
+        API.getUsers().then(results => {
+            console.log(results.data.results)
+            this.setState({
+                users: results.data.results,
+                filteredUsers: results.data.results
+            })
+        })
 
-handleSort=heading=>{
-    
-}
+    }
+
+    handleSort = heading => {
+        if ( state.order === "descend" ) {
+            setState ({
+                order: "ascend"
+            })
+        } else {
+            setState ({
+                order: "descend"
+            })
+        }
+
+    }
     handleSearchChange = event => {
         const filter = event.target.value;
-        const filteredList = this.state.users.filter( item => { 
+        const filteredList = this.state.users.filter(item => {
             let values = item.name.first.toLowerCase();
             return values.indexOf(filter.toLowerCase()) !== -1;
         })
-        this.setState({filteredUsers:filteredList})
+        this.setState({ filteredUsers: filteredList })
     }
 
     render() {
         return (
             <div>
-                <Navbar handleSearchChange={this.handleSearchChange}/>
+                <Navbar handleSearchChange={this.handleSearchChange} />
                 <DataTable
-                headings={this.headings}
-                users={this.state.filteredUsers}
-                handleSort={this.handleSort}
+                    headings={this.headings}
+                    users={this.state.filteredUsers}
+                    handleSort={this.handleSort}
                 />
             </div>
         )
